505ff3689d592b95edd80dc9aed010c06ef72c3b
riya1945 <riya.ananya.joshi@gmail.com>
2025-08-02 15:29:31 +0530

    Modified the Model

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,10 +5,8 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-# Load model and features
-model = joblib.load("backend/model/model.pkl")
-feature_columns = joblib.load("backend/model/feature_columns.pkl")
+model = joblib.load("../model/model.pkl")
+feature_columns = joblib.load("../model/feature_columns.pkl")
 
 class InputData(BaseModel):
     features: list[float]
@@ -18,15 +16,17 @@ def root():
     return {"NovaVitals is now live!"}
 
 @app.post("/predict")
+@app.post("/predict")
 def predict(data: InputData):
+    
     input_df = pd.DataFrame([data.features], columns=feature_columns)
     prediction = model.predict(input_df)[0]
     return {"prediction": prediction}
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 🔓 Allow all origins (for development)
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"],  
 )
